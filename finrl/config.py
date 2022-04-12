d5882c1829bef621026badc0f7d1a836d5815f87
5neaX <57161419+5neaX@users.noreply.github.com>
2022-04-13 03:55:10 +1000

    2014 changed to 2016 in train

--- finrl/config.py
+++ finrl/config.py
@@ -4,8 +4,8 @@ TRAINED_MODEL_DIR = "trained_models"
 TENSORBOARD_LOG_DIR = "tensorboard_log"
 RESULTS_DIR = "results"
 
-# date format: '%Y-%m-%d'
-TRAIN_START_DATE = "2014-01-01"
+# date format: '%Y-%m-%d6
+TRAIN_START_DATE = "2016-01-01"
 TRAIN_END_DATE = "2020-07-31"
 
 TEST_START_DATE = "2020-08-01"
